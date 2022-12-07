535fdc7a670ea40593abe9b23ed132fa2c91b3a9
Shaik-Chapal <shaikchapal@hotmail.com>
2022-12-07 16:17:55 +0200

    theme setup for login page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById("root"))
+
+
+ReactDOM.render(
+    <Router>
+       <Routes>
+            <Route path= "/" element= {<Login/>}></Route>
+
+       </Routes>
+    </Router>
+    ,document.getElementById("root"))
