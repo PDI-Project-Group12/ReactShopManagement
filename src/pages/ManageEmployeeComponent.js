95913c1dbe33b19ce9965633d357762f74e00567
Shaik-Chapal <shaikchapal@hotmail.com>
2022-12-20 13:24:57 +0200

    employee data

--- src/pages/ManageEmployeeComponent.js
+++ src/pages/ManageEmployeeComponent.js
@@ -17,14 +17,14 @@ class ManageEmployeeComponent extends React.Component{
             <section className="content">
                 <div className="container-fluid">
                     <div className="block-header">
-                        <h2>MANAGE PRODUCT DATA</h2>
+                        <h2>MANAGE EMPLOYEE DATA</h2>
                     </div>
                     <div className="row clearfix">
                 <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div className="card">
                         <div className="header">
                             <h2>
-                                Add Product
+                                Add Employee Data
                             </h2>
                             
                         </div>
@@ -35,15 +35,23 @@ class ManageEmployeeComponent extends React.Component{
                                     <div className="form-line">
                                         <input type="text" id="name" 
                                         name="name"
-                                        className="form-control" placeholder="Enter Company Name"/>
+                                        className="form-control" placeholder="Enter Employee Name"/>
                                     </div>
                                 </div>
-                                <label htmlFor="email_address">License No.</label>
+                                <label htmlFor="email_address">Employee Id</label>
                                 <div className="form-group">
                                     <div className="form-line">
-                                        <input type="text" id="license_no" 
-                                        name="license_no"
-                                        className="form-control" placeholder="Enter License No."/>
+                                        <input type="text" id="id" 
+                                        name="id"
+                                        className="form-control" placeholder="Enter Employee Id"/>
+                                    </div>
+                                </div>
+                                <label htmlFor="email_address">Joining Date</label>
+                                <div className="form-group">
+                                    <div className="form-line">
+                                        <input type="text" id="joining_date" 
+                                        name="joining_date"
+                                        className="form-control" placeholder="Enter Joining Date "/>
                                     </div>
                                 </div>
                                 <label htmlFor="email_address">Address</label>
@@ -51,7 +59,7 @@ class ManageEmployeeComponent extends React.Component{
                                     <div className="form-line">
                                         <input type="text" id="address" 
                                         name="address"
-                                        className="form-control" placeholder="Enter Company Address"/>
+                                        className="form-control" placeholder="Enter Employee Address"/>
                                     </div>
                                 </div>
                                 <label htmlFor="email_address">Contact No.</label>
@@ -70,18 +78,11 @@ class ManageEmployeeComponent extends React.Component{
                                         className="form-control" placeholder="Enter Company Email"/>
                                     </div>
                                 </div>
-                                <label htmlFor="email_address">Description</label>
-                                <div className="form-group">
-                                    <div className="form-line">
-                                        <input type="text" id="description" 
-                                        name="description"
-                                        className="form-control" placeholder="Enter Company Description"/>
-                                    </div>
-                                </div>
+                                
                                 
                         
                                 <br/>
-                                <button type="submit" className="btn btn-primary m-t-15 waves-effect-btn-block">Add Product</button>
+                                <button type="submit" className="btn btn-primary m-t-15 waves-effect-btn-block">Add Employee Data</button>
                             </form>
                         </div>
                     </div>
